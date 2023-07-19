e06b72b108fde95c01c2aeb87ccb7f7330ab5b8d
martaalcazarr <martaalcazarredondo@gmail.com>
2023-07-18 21:32:14 -0400

    first commit

---
+++ db/migrate/20230719012448_create_pictures.rb
@@ -0,0 +1,11 @@
+class CreatePictures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pictures do |t|
+      t.string :name
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
